10a323781d2647d723668e45c5a3a4167ee1c8ab
Kevin Nguyen <kevin.nguyen2321@yahoo.com>
2025-09-09 20:27:28 -0500

    Completed registrating and logging in functionality

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,20 +1,21 @@
-import { Geist, Geist_Mono } from 'next/font/google';
-import './globals.css';
-import Navbar from './components/Navbar';
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Navbar from "./components/Navbar";
+import { AuthProvider } from "@/contexts/AuthContext";
 
 const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
 
 export const metadata = {
-  title: 'ConcertTrak',
-  description: 'Track your concert experiences',
+  title: "ConcertTrak",
+  description: "Track your concert experiences",
 };
 
 export default function RootLayout({ children }) {
@@ -23,8 +24,10 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <main className="pb-20">{children}</main>
-        <Navbar />
+        <AuthProvider>
+          <main className="pb-20">{children}</main>
+          <Navbar />
+        </AuthProvider>
       </body>
     </html>
   );
